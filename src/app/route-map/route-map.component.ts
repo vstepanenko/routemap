61bd5061f46f4bad2aaa0afae48ba747adce1a20
Viktor Stepanenko <vitya.stepanenko@gmail.com>
2018-07-12 03:09:38 +0300

    implemented basic database reading and route drawing functionality

--- src/app/route-map/route-map.component.ts
+++ src/app/route-map/route-map.component.ts
@@ -1,5 +1,21 @@
 import {Component, OnInit} from '@angular/core';
 import {RouteService} from '../services/route.service';
+import {firestore} from 'firebase';
+import GeoPoint = firestore.GeoPoint;
+
+class RoutePoint {
+  geo: GeoPoint;
+}
+
+class Route {
+  points: RoutePoint[];
+  opacity?: number;
+
+  constructor(points: RoutePoint[], opacity: number = 0.4) {
+    this.points = points;
+    this.opacity = opacity
+  }
+}
 
 @Component({
   selector: 'route-map',
@@ -7,20 +23,31 @@ import {RouteService} from '../services/route.service';
   styleUrls: ['./route-map.component.css']
 })
 export class RouteMapComponent implements OnInit {
+  show: boolean;
   mapConfig = {
     latitude: 33.0,
     longitude: 33.0
   };
+  routes: Route[] = [];
 
   constructor(private routeService: RouteService) {
-
   }
 
   ngOnInit() {
-    this.routeService.getAll().subscribe(this.updateRoutes)
+    this.routeService.getAll().subscribe(data => this.updateRoutes(data));
+  }
+
+  mouseOver(route: Route) {
+    route.opacity = 1;
+  }
+
+  mouseOut(route: Route) {
+    route.opacity = 0.4;
   }
 
   private updateRoutes(data: any[]) {
-    console.log(data);
+    this.routes = data.map(x => new Route(x));
+    this.show = true;
+    console.log(this.routes);
   }
 }
--- src/app/services/route.service.ts
+++ src/app/services/route.service.ts
@@ -1,16 +1,31 @@
 import {Injectable} from '@angular/core';
-import {AngularFirestore} from 'angularfire2/firestore';
-import {Observable} from 'rxjs';
+import {AngularFirestore, AngularFirestoreCollection} from 'angularfire2/firestore';
+import {combineLatest, Observable} from 'rxjs';
+import {mergeMap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RouteService {
+  private routeCollection: AngularFirestoreCollection<any>;
 
-  constructor(private db: AngularFirestore) {
+  constructor(private afs: AngularFirestore) {
+    this.routeCollection = afs.collection('routes');
   }
 
-  getAll() {
-    return this.db.collection('routes').valueChanges();
+  getAll(): Observable<any> {
+    return this.routeCollection.snapshotChanges().pipe(
+      mergeMap(routes => {
+        const routeMap = routes.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        });
+        console.log(routeMap);
+        const items: Observable<any>[] = [];
+        routeMap.forEach(x => items.push(this.afs.collection('routes/' + x.id + '/points').valueChanges()));
+        return combineLatest(...items);
+      })
+    )
   }
 }
